bdf529347c8201b31fecbd5457e9fa6d9b53b1c1
hannaabdou <hanaabdou743@gmail.com>
2023-07-18 13:08:23 -0700

    git commit git add . git commit -m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,11 @@
 #include "main.h"
-
-
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
-
-
-	void print_alphabet(void)
-	{
-		int ch;
-
-
-		for (ch = 'a'; ch <= 'z'; ch++)
-			_putchar(ch);
-
-
-		_putchar('\n');
-	}
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
+void print_alphabet(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
