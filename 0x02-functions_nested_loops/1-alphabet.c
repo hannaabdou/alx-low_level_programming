cebe48289d6e920f99712a7f1ca2716533ad0853
hannaabdou <hanaabdou743@gmail.com>
2023-07-18 11:37:26 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,11 @@
 
 	void print_alphabet(void)
 	{
-		char letter;
+		int ch;
 
 
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
 
 
 		_putchar('\n');
